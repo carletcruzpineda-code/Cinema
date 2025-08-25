092c02cf021b87d5cb019a514727f68cba8e598d
ccruz1234 <ccruzpinedafwd@gmail.com>
2025-08-25 15:53:13 -0600

    first commit

---
+++ Public/js/cartelera.js
@@ -0,0 +1,8 @@
+/* Selecting Elements from the DOM  */
+let Search= document.getElementById("Search");
+let poster = document.getElementById("poster");
+let filmgenre = document.getElementById("filmgenre");
+let releaseyear = document.getElementById("releaseyear");
+let btnAgregar = document.getElementById("btnAgregar");
+
+
